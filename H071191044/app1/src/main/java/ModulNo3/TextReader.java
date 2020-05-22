b07f5951ad2ef61b283a4783bdc8b058c933bb74
YusufSyam <54948391+YusufSyam@users.noreply.github.com>
2020-05-22 23:01:07 +0800

    Upload Praktikum 8
    Direktori tugas praktikum per nomor sesuai folder/package masing-masing (misal untuk no 1, maka berada di app1/src/main/java/ModulNo1)

---
+++ H071191044/app1/src/main/java/ModulNo3/TextReader.java
@@ -0,0 +1,70 @@
+package ModulNo3;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class TextReader extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        VBox root = new VBox();
+        root.setPadding(new Insets(10));
+        root.setSpacing(10);
+        root.setAlignment(Pos.TOP_CENTER);
+
+        FileChooser fc = new FileChooser();
+
+        TextArea textArea = new TextArea();
+        textArea.setMinHeight(350);
+
+        Button button = new Button("Select First File");
+
+        button.setOnAction(e -> {
+            fc.setTitle("Select File");
+            fc.setInitialDirectory(new File("C:/"));
+            fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text Files", "*txt"));
+
+            File file = fc.showOpenDialog(stage);
+            StringBuilder sb = readFile(file);
+            textArea.setText(sb.toString());
+
+        });
+		
+        root.getChildren().add(button);
+        root.getChildren().add(textArea);
+        Scene scene = new Scene(root, 400, 410);
+        stage.setTitle("Text Reader");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private StringBuilder readFile(File file2) {
+        StringBuilder sb = new StringBuilder();
+        String s = " ";
+        try {
+            FileReader fileReader = new FileReader(file2);
+            BufferedReader reader = new BufferedReader(fileReader);
+            while ((s = reader.readLine()) != null) {
+                sb.append(s).append("\n");
+            }
+        } catch (Exception e) {
+            e.getMessage();
+        }
+        return sb;
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
