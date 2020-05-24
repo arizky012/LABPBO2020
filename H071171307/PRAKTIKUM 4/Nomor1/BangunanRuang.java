c4e118aa90b1329ea8d4190a477fd72a92774fe0
ayufarahdiba <61412025+ayufarahdiba@users.noreply.github.com>
2020-05-24 19:57:45 +0700

    Add files via upload

---
+++ H071171307/PRAKTIKUM 4/Nomor1/BangunanRuang.java
@@ -0,0 +1,9 @@
+public class BangunanRuang {
+
+    public double Volume(){
+        return 0;
+    }
+    public double LuasPermukaan(){
+        return 0;
+    }
+}
---
+++ H071171307/PRAKTIKUM 4/Nomor1/Segitiga.java
@@ -0,0 +1,15 @@
+public class Segitiga extends BangunDatar{
+
+    double a,t,s;
+
+    @Override
+    public double Luas() {
+        double luas = (a*t)/2;
+        return luas;
+    }
+    @Override
+    public double Keliling() {
+        double keliling = a+t+s;
+        return keliling;
+    }
+}
